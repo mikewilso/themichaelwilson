6e01c027f0399c0120988ff9809f0edd755d9d66
Michael Wilson <miwilson@sovrn.com>
2018-06-12 14:46:05 -0600

    Added welcome page with Bootstrap, API functionality

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 # from flask import Flask, render_template
 import flask
 app = flask.Flask(__name__)
+import json
+
+site_name = 'themichaelwilson.com'
+
 
 @app.route('/')
-def main_app():
-    return flask.render_template('index.html')
+def index():
+    flask.url_for('static', filename='css/bootstrap.min.css')
+    return flask.render_template('index.html', site_name=site_name)
+
+@app.route('/api/test')
+def api_test():
+    sample_obj = {
+        "name": "Michael Wilson",
+        "age": 28,
+        "occupation":"Support Engineer"
+    }
+    return app.response_class(
+        response=json.dumps(sample_obj),
+        status=200,
+        mimetype='application/json'
+    )
 
 if __name__ == "__main__":
     app.run()
