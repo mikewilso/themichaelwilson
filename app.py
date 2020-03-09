5ed1fd5556ffef126133a29e8162a0a18f01cc3d
Michael Wilson <miwilson@sovrn.com>
2020-03-09 15:44:04 -0600

    fixed validation endpoint

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def commerce_sticky():
     flask.url_for('static', filename='css/bootstrap.min.css')
     return flask.render_template('commerceconnectstickyfooter.html')
 
-@app.route("/.well-known/pki-validation")
+@app.route("/.well-known/pki-validation/632BDE138705220EFF83EE038DC5A0FE.txt")
 def file_sender():
     return flask.send_file('static/632BDE138705220EFF83EE038DC5A0FE.txt')
 
